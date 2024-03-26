2cb32b5e7738f46060ebb5325d7f00bde7b89fa3
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-27 01:39:35 +0300

    Should not be executed when imported.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,17 +5,18 @@ Provides some stats about Nginx logs stored in MongoDB.
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
-nginx = db.nginx
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    nginx = db.nginx
 
-print(f"{nginx.count_documents({})} logs")
+    print(f"{nginx.count_documents({})} logs")
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("Methods:")
-for method in methods:
-    result = nginx.count_documents({"method": method})
-    print(f"\tmethod {method}: {result}")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        result = nginx.count_documents({"method": method})
+        print(f"\tmethod {method}: {result}")
 
-status_check = nginx.count_documents({"method": "GET", "path": "/status"})
-print(f"{status_check} status check")
+    status_check = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")

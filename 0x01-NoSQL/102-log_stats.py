fabd913040babd7cc8025af9d5b46afc2b034a43
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-27 03:48:36 +0300

    Log stats

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Log stats - new version.
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    nginx = db.nginx
+
+    print(f"{nginx.count_documents({})} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        result = nginx.count_documents({"method": method})
+        print(f"\tmethod {method}: {result}")
+
+    status_check = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+    ip_list = nginx.distinct("ip")

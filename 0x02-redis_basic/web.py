29a65d6a20850f173ccf5e7e9eb168301103cb30
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-01 01:33:39 +0300

    Expiring web cache

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Implenting an expiring web cache and tracker.
+"""
+from functools import wraps
+import requests
+import redis
+
+
+def count_requests(method):
+    """
+    Tracks how many times a particular URL was accessed.
+    """
+    @wraps(method)
+    def wrapper(url):
+        """
+        Wrapper function.
+        """
+        connection = redis.Redis()
+        connection.incr(f"count:{url}")
+        connection.expire(f"count:{url}", 10)
+        print(connection.get(f"count:{url}"), connection.ttl(f"count:{url}"))
+        return method(url)
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """
+    Get's html content of the given url.
+    """
+    content = requests.get(url).text
+    return content

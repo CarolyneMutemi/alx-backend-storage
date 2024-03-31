88165d8fdccd50456c5e1d2280b3404308f133ef
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-01 02:40:21 +0300

    Implemented the caching in the get_page function instead of in a decorator function.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,36 +2,43 @@
 """
 Implenting an expiring web cache and tracker.
 """
-from functools import wraps
 import requests
 import redis
 
 
-def count_requests(method):
-    """
-    Tracks how many times a particular URL was accessed.
-    """
-    @wraps(method)
-    def wrapper(url):
-        """
-        Wrapper function.
-        """
-        connection = redis.Redis()
-        page = connection.get("page")
-        if not page:
-            page = method(url)
-            connection.setex("page", 10, page)
-        else:
-            page = page.decode('utf-8')
-        connection.incr(f"count:{url}")
-        return page
-    return wrapper
+# def count_requests(method):
+#     """
+#     Tracks how many times a particular URL was accessed.
+#     """
+#     @wraps(method)
+#     def wrapper(url):
+#         """
+#         Wrapper function.
+#         """
+#         connection = redis.Redis()
+#         page = connection.get("page")
+#         if not page:
+#             page = method(url)
+#             connection.setex("page", 10, page)
+#         else:
+#             page = page.decode('utf-8')
+#         connection.incr(f"count:{url}")
+#         return page
+#     return wrapper
 
 
-@count_requests
 def get_page(url: str) -> str:
     """
     Get's html content of the given url.
     """
-    content = requests.get(url, timeout=5).text
-    return content
+    connection = redis.Redis()
+    page = connection.get("page")
+    if not page:
+        page = requests.get(url, timeout=5).text
+        connection.setex("page", 10, page)
+    else:
+        page = page.decode('utf-8')
+    connection.incr(f"count:{url}")
+    print(connection.get(f"count:{url}"))
+    print(page)
+    return page
